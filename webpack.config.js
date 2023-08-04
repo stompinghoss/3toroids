f71e6e974f8e7899ba86bed652a83db4cfa192df
fieldp <fieldp@hey.com>
2023-08-04 13:14:41 +0100

    tweaked build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/3toroids.js',
+  entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
 plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: '3 toroids',
       template: './src/index.html', // Path to your template file
