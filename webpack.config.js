1e150345f0fc7683a084535ed8341662db381f52
fieldp <fieldp@hey.com>
2023-08-12 12:24:18 +0100

    tidy up

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
 
 module.exports = {
   entry: './src/main.js',
@@ -28,6 +29,11 @@ module.exports = {
       patterns: [
         { from: './assets', to: 'assets' }, // adjust 'src/assets' and 'assets' as needed
       ],
+    }),
+    new LicenseWebpackPlugin({
+      // options go here, e.g.:
+      perChunkOutput: false, // set this to true if you want one file per chunk
+      outputFilename: '[name].licenses.txt',
     }),
   ],
 };
